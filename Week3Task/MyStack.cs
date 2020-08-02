bd7458a0762aa3c0ec49651fadfc8f8b71bb1ecd
adeola <adeola.idowu@decagon.dev>
2020-08-02 19:45:18 +0100

    Add project files.

---
+++ Week3Task/MyStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week3Task
+{
+    class MyStack<T>
+    {
+        List<T> Stack = new List<T>();
+        
+        public bool isEmpty()
+        {
+            return Stack.Count == 0;
+        }
+
+        public void Push(T value)
+        {
+            Stack.Add(value);
+        }
+
+        public void Print()
+        {
+            for (int i = Stack.Count -1;  i >= 0; i--)
+            {
+                Console.WriteLine(Stack[i]);
+            }
+        }
+
+        public T Pop()
+        {
+            T Removed = Stack[Stack.Count - 1];
+            Stack.RemoveAt(Stack.Count - 1);
+            return Removed;
+        }
+
+        public T Peek()
+        {
+            T Last = Stack[Stack.Count - 1];
+            return Last;
+        }
+
+        public int Size()
+        {
+            return Stack.Count;
+        }
+    }
+}
---
+++ Week3Task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Week3Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> testStack = new MyStack<int>();
+            Console.WriteLine(testStack.isEmpty());
+            testStack.Push(4);
+            testStack.Push(9);
+            testStack.Push(35);
+            Console.WriteLine(testStack.isEmpty());
+            testStack.Print();
+            Console.WriteLine();
+            Console.WriteLine(testStack.Pop());
+            Console.WriteLine();
+            testStack.Print();
+            Console.WriteLine(testStack.Size());
+            Console.WriteLine(testStack.Peek());
+
+        }
+    }
+}
